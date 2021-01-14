c6cc21c6fde2b33804645fda875f3aa2e14e1a5a
Franyeliza <franyelizarodriguez@live.com>
2021-01-13 22:36:37 -0400

    Backend enviroment done

---
+++ frontend/src/Components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => (
+  <h1>Frontend tools ready</h1>
+);
+
+ReactDom.render(<App />, document.getElementById('root'));
